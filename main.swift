f429a149791e52331e336cfe8c9aa89a5f812d70
Newton Triumphant <funkeudo@outlook.com>
2020-11-29 21:12:14 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,42 @@
+// Person declaration
+let firstName = "Name: Bob"
+let lastName = "Smith"
+let fullName = firstName + " " + lastName // String concatenation
+var age = "Age: 51"
+var weight = "Weight: 212.12 pounds" 
+var isOrganDonor = "Organ Donor: No"
+let eyeColor = "Eye color: Blue"
+var hairColor = "Hair color: Dark brown"
+
+print("------------")
+
+// Title
+print("About my boss")
+
+print("")
+
+// Information
+print(fullName)
+print(age)
+print(weight)
+print(isOrganDonor)
+print(eyeColor)
+print(hairColor)
+
+print("")
+
+// New weight
+print("Bob took a workout yesterday.")
+weight = "As of today, he weighs 200.35 pounds"
+print(weight)
+
+print("")
+
+// Dyed hair
+var message1: String = "He dyed his hair yesterday." // Explicit type declaration
+hairColor = "His hair is now black." // Redefining an inference type declaration
+
+print(message1)
+print(hairColor)
+
+print("------------")
